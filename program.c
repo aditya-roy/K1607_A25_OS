016c3931bf2108dbbd5960cb3c5c5729909e7939
Aditya Roy <adityaroy456@gmail.com>
2018-03-20 21:49:22 +0530

    third
    created main function.
    created threads using array and joined them.

--- program.c
+++ program.c
@@ -16,4 +16,29 @@ printf("CAR %d is unlocked\n\n", CAR);
 void intersection(){
 	printf("------------Car is in Traffic Intersection------------\n\n");
 }
-
+main()
+{
+	int n,i;
+  	printf("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+  	printf("               Traffic Intersection Problem                  \n");
+  	printf("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+  	printf("North :N\nSouth :S\nEast :E\nWest :W \n\n");
+  	pthread_mutex_init(&NW,NULL);
+  	pthread_mutex_init(&NE,NULL);
+  	pthread_mutex_init(&SE,NULL);
+  	pthread_mutex_init(&SW,NULL);
+printf("Enter No Of Cars\n");
+scanf("%d",&n);
+int cid[n];
+pthread_t c[n];
+for(i = 0; i <n; i++)
+{
+cid[i] = i;
+ pthread_create(&c[i], NULL, criticalsection,(void*)&cid[i]);
+ sleep(1);
+}
+for(i=0;i<n;i++)
+{
+pthread_join(c[i],NULL);
+}
+}
