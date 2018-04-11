4f8ff17bceff68bbc4620c836c9547de734433fd
Aditya Roy <adityaroy456@gmail.com>
2018-04-11 23:29:24 +0530

    Fifth
    created straight, leftturn, rightturn function

--- program.c
+++ program.c
@@ -16,6 +16,108 @@ printf("CAR %d is unlocked\n\n", CAR);
 void intersection(){
 	printf("------------Car is in Traffic Intersection------------\n\n");
 }
+static void straight(int carorigin, int cn){
+if (carorigin==0){
+lock(cn,NE);
+lock(cn,SE);
+printf("Car %d is moving from South to North\n\n", cn);
+intersection();
+unlock(cn,SE);
+unlock(cn,NE);
+}
+else if (carorigin==1){
+lock(cn,NW);
+lock(cn,SW);
+printf("Car %d is moving from North to South\n\n", cn);
+intersection();
+unlock(cn,SW);
+unlock(cn,SW);
+}
+else if (carorigin==2){
+lock(cn,SE);
+lock(cn,SW);
+printf("Car %d is moving from West to East\n\n", cn);
+intersection();
+unlock(cn,SW);
+unlock(cn,SE);
+}
+else if (carorigin==3){
+lock(cn,NW);
+lock(cn,NE);
+printf("Car %d is moving from East to West\n\n", cn);
+intersection();
+unlock(cn,NE);
+unlock(cn,NW);
+}
+}
+static void leftturn(int carorigin, int cn){
+if(carorigin==0){
+lock(cn,NE);
+lock(cn,SE);
+lock(cn,SW);
+printf("Car %d is moving from South to West\n", cn);
+intersection();
+unlock(cn,SW);
+unlock(cn,SE);
+unlock(cn,NE);
+}
+else if(carorigin==1){
+lock(cn,NW);
+lock(cn,NE);
+lock(cn,SW);
+printf("Car %d is moving from North to East\n", cn);
+intersection();
+unlock(cn,SW);
+unlock(cn,NE);
+unlock(cn,NW);
+}
+else if(carorigin==2){
+lock(cn,NW);
+lock(cn,SE);
+lock(cn,SW);
+printf("Car %d is moving from West to North\n", cn);
+intersection();
+unlock(cn,SE);
+unlock(cn,NE);
+unlock(cn,NW);
+}
+else if(carorigin==3){
+lock(cn,NW);
+lock(cn,SE);
+lock(cn,SW);
+printf("Car %d is moving from East to South\n", cn);
+intersection();
+unlock(cn,SE);
+unlock(cn,NE);
+unlock(cn,NW);
+}
+}
+static void rightturn(int carorigin, int cn){
+if(carorigin==0){
+lock(cn,NE);
+printf("Car %d is moving from South to East\n", cn);
+intersection();
+unlock(cn,NE);
+}
+else if(carorigin==1){
+lock(cn,SW);
+printf("Car %d is moving from North to West\n", cn);
+intersection();
+unlock(cn,NE);
+}
+else if(carorigin==2){
+lock(cn,SW);
+printf("Car %d is moving from West to South\n", cn);
+intersection();
+unlock(cn,NE);
+}
+else if(carorigin==3){
+lock(cn,SE);
+printf("Car %d is moving from East to North\n", cn);
+intersection();
+unlock(cn,NW);
+}
+}
 static void * criticalsection(void* arg){
 int * carnumberptr;
 int cn;
